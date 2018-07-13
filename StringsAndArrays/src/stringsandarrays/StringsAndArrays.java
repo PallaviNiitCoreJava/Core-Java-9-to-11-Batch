0ee61f72722b75ec868709b1ad476965aefac3b9
PallaviNiitCoreJava <37013933+PallaviNiitCoreJava@users.noreply.github.com>
2018-07-13 10:37:58 +0530

    Add files via upload

---
+++ StringsAndArrays/src/stringsandarrays/StringsAndArrays.java
@@ -0,0 +1,30 @@
+package stringsandarrays;
+
+import java.util.Scanner;
+
+public class StringsAndArrays {
+
+    public static void main(String[] args) {
+
+//        int a=10;
+//        Integer b=new Integer(5);
+//        a=b;
+//        System.out.println("a= "+a);
+//        System.out.println("b= "+b);
+        Scanner sc = new Scanner(System.in);
+        int id[] = new int[5];
+        int tot=0;
+        //Integer roll[]=new Integer[5];
+        //Double d[]=new Double[2];
+        for (int i = 0; i < id.length; i++) {
+            System.out.println("Enter a Number : ");
+            id[i] = sc.nextInt();
+            tot=tot+id[i];
+        }
+        for (int i = 0; i < id.length; i++) {
+            System.out.println("id = " + id[i]);
+        }
+        System.out.println("Total is "+tot);
+    }
+
+}
